3a378164f271edfec8f4446aa086f6e98462d844
Antti Haapala <antti@haapala.name>
2014-08-21 10:38:04 +0300

    zodb support

---
+++ tet/zodb/password.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division,\
+       print_function, unicode_literals
+
+import sqlalchemy as sa
+from sqlalchemy import orm as orm
+from sqlalchemy.ext import declarative
+
+from ..util.crypt import crypt, verify
+
+class UserPasswordMixin(object):
+    _password = None
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = crypt(password)
+
+    def validate_password(self, password):
+        if self._password is None:
+            return False
+
+        return verify(password, self._password)
