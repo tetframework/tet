20c64225ba7627c316cc5f5bca585598c92c1a49
Antti Haapala <antti.haapala@anttipatterns.com>
2015-05-02 12:49:31 +0300

    sqlarootfactory

---
+++ tet/sqlalchemy/factory.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
+
+class SQLARootFactory(object):
+    def __init__(self, request):
+        self.request = request
+
+    def __getitem__(self, item):
+        try:
+            return self.supplier(item)
+        except (MultipleResultsFound, NoResultFound) as e:
+            raise KeyError(item) from e
