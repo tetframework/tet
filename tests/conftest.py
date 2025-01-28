2a7577002878a1fcde9690e10b9e3a1201c2ebd5
longnguyen <long.nguyendr@gmail.com>
2025-01-28 15:19:46 +0200

    Update conftest:
    - Implementing the secrect, and login callback(s)
    - Adding the route_prefix for callable module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
+import logging
+from functools import wraps
+
 import pytest
+from pyramid.httpexceptions import HTTPForbidden
 from pyramid.request import Request
 from pyramid.security import Allow, Authenticated, Everyone, Deny
 from pyramid.testing import setUp, tearDown
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, or_
 from sqlalchemy.orm import Session
 
 from tests.models.accounts import Base, Token, User
@@ -10,6 +14,8 @@ from tet.config import Configurator as tetConfigurator
 
 DB_NAME = "test_tet"
 DB_URL = f"postgresql:///{DB_NAME}"
+
+logger = logging.getLogger(__name__)
 
 
 def create_test_database():
@@ -29,6 +35,8 @@ def db_engine(database):
     engine = create_engine(DB_URL)
     Base.metadata.create_all(engine)
     yield engine
+    # TODO: Dropping all entities will disrupt the saving of tokens in the security/authentication module.
+    #  Investigate the workflow and resolve the issue.
     # Base.metadata.drop_all(engine)
     engine.dispose()
 
@@ -45,14 +53,25 @@ def db_session(db_engine, pyramid_request, transaction_manager):
         yield session
 
 
-def authentication_callback(request: Request) -> User.id:
-    # TODO: Implement the actual callback here.
-    return 1
+# don't actually print the logger for this callback
+def login_callback(request: Request) -> User.id:
+    """This is just an example of a login callback. It should be defined by the pyramid app."""
+    db_session = request.find_service(Session)
+    payload = request.json_body
+    # user_identity here could be an email, or username
+    user_identity = payload['user_identity']
+    user = (db_session.query(User)
+            .filter(or_(User.email == user_identity,
+                        User.name == user_identity))
+            .first())
+    if not user:
+        return None
+    return user.id
 
 
 def secret_callback(request: Request) -> str:
-    # TODO: Get it from the settings or elsewhere
-    return "secret"
+    """Get it from the settings or elsewhere"""
+    return request.registry.settings['tet.security.authentication.secret']
 
 
 @pytest.fixture()
@@ -62,15 +81,18 @@ def pyramid_request(pyramid_app, db_engine):
         yield request
     tearDown()
 
+
 class RootFactory(object):
     __acl__ = [
         (Allow, Authenticated, 'view'),
         (Allow, 'group:editors', 'edit'),
+        (Allow, Everyone, 'login'),
         (Deny, Everyone, 'delete')
     ]
 
     def __init__(self, request):
         self.request = request
+
 
 @pytest.fixture()
 def pyramid_app(db_engine):
@@ -79,6 +101,7 @@ def pyramid_app(db_engine):
         'sqlalchemy.url': DB_URL,
         "project_prefix": "tet",
         "pyramid.includes": ["pyramid_tm"],
+        "tet.security.authentication.secret": "secret",
     }
     with tetConfigurator() as config:
         config.add_settings(settings)
@@ -87,11 +110,11 @@ def pyramid_app(db_engine):
         config.include("pyramid_di")
         config.setup_sqlalchemy(engine=db_engine)
         config.set_root_factory(RootFactory)
-        config.include("tet.security.authentication")
+        config.include("tet.security.authentication", route_prefix="/api/v1/auth")
         config.tet_configure_authentication_token(
             token_model=Token,
             project_prefix=config.registry.settings['project_prefix'],
-            user_verification_callback=authentication_callback,
+            login_callback=login_callback,
             secret_callback=secret_callback,
         )
         config.add_route('home', '/')
