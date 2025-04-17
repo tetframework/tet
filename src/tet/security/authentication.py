f14e999af7bcb5b4e05e746c0664d10d50432736
longnguyen <long.nguyendr@gmail.com>
2025-04-17 16:13:49 +0300

    Set the cookie value from the response header if the cookie_attributes instance is present.

--- src/tet/security/authentication.py
+++ src/tet/security/authentication.py
@@ -775,6 +775,8 @@ class AuthViews:
         response = self.login()
         if isinstance(response, dict) and response.get("mfa_required"):
             return response
+
+        self.cookie_attributes.value = self.response.headers[self.long_term_token_header]
         self._set_cookie(
             cookie_attrs=self.cookie_attributes
             or CookieAttributes(
@@ -807,6 +809,7 @@ class AuthViews:
             isinstance(self.security_policy, JWTCookieAuthenticationPolicy)
             and self.request.matched_route.name == "tet_auth_mfa_challenge"
         ):
+            self.cookie_attributes.value = self.response.headers[self.long_term_token_header]
             self._set_cookie(
                 cookie_attrs=self.cookie_attributes
                 or CookieAttributes(
